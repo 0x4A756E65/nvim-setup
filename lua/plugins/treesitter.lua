e14cb408209f31608c7d2b6551fb16cc7d2b033c
Aaron Stone <apstone@protonmail.com>
2025-04-03 21:04:09 -0400

    update to snacks

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua", "javascript", "typescript", "graphql", "json"},
+        ensure_installed = {"lua", "javascript", "typescript", "graphql", "json", "regex"},
         highlight = { enable = true },
         indent = { enable = true }
       })
